598146c5dc53f203ff434aec7d3fa206618b4419
UP938212 <72561290+UP938212@users.noreply.github.com>
2021-03-12 14:14:04 +0000

    pro
    pro

--- PlagiarismChecker.js
+++ PlagiarismChecker.js
@@ -1,5 +1,5 @@
-'use strict';
-function SourceCodeAcceptor(){
-    return "hello";
-}
+const input = document.querySelector('input[type="file"]')
+input.addEventListener('change', function (e) {
+    console.log(input.files)
+}, false)
 
